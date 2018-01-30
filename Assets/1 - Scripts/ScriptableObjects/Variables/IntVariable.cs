1657b83feab931f77cd495375b77bef721b85226
Wodopo <pedrocoelhotv@gmail.com>
2018-01-30 18:11:58 -0200

    Added Fruit pickup

--- Assets/1 - Scripts/ScriptableObjects/Variables/IntVariable.cs
+++ Assets/1 - Scripts/ScriptableObjects/Variables/IntVariable.cs
@@ -1,6 +1,17 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 [CreateAssetMenu]
 public class IntVariable : ScriptableVariable<int>
 {
+    public void Add(int value)
+    {
+        value = Mathf.Abs(value);
+        Value += value;
+    }
+
+    public void Remove(int value)
+    {
+        value = Mathf.Abs(value);
+        Value -= value;
+    }
 }
